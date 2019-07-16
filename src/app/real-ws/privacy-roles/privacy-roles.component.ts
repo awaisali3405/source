514557287e1dcd672e58089101c4dca644c43ca7
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2019-07-16 14:02:54 +0200

    Basic application of the technique

--- src/app/real-ws/privacy-roles/privacy-roles.component.ts
+++ src/app/real-ws/privacy-roles/privacy-roles.component.ts
@@ -15,10 +15,13 @@ import {Router} from '@angular/router';
 export class PrivacyRolesComponent implements OnInit {
   sanitizer: DomSanitizer;
   pm4pyService: Pm4pyService;
+  public technique : string;
 
   constructor(private _sanitizer: DomSanitizer, private pm4pyServ: Pm4pyService, private authService: AuthenticationServiceService, public dialog: MatDialog, private _route : Router) {
     this.pm4pyService = pm4pyServ;
     this.sanitizer = _sanitizer;
+
+    this.technique = "fixed_value";
 
     this.authService.checkAuthentication().subscribe(data => {
     });
@@ -28,7 +31,25 @@ export class PrivacyRolesComponent implements OnInit {
   }
 
   applyFilter() {
+    let no_substitutions = (<HTMLInputElement>document.getElementById("no_substitutions")).value;
+    let selective_lower_bound_applied = true;
+    let selective_upper_bound_applied = true;
+    let fixed_value = (<HTMLInputElement>document.getElementById("fixed_value")).value;
+    let resource_aware = true;
+    let hashed_activities = true;
+    let event_attributes2remove = "";
+    let trace_attributes2remove = "";
+
     let httpParams : HttpParams = new HttpParams();
+    httpParams = httpParams.set("no_substitutions", ""+no_substitutions);
+    httpParams = httpParams.set("selective_lower_bound_applied", ""+selective_lower_bound_applied);
+    httpParams = httpParams.set("selective_upper_bound_applied", ""+selective_upper_bound_applied);
+    httpParams = httpParams.set("fixed_value", ""+fixed_value);
+    httpParams = httpParams.set("technique", ""+this.technique);
+    httpParams = httpParams.set("resource_aware", ""+resource_aware);
+    httpParams = httpParams.set("hashed_activities", ""+hashed_activities);
+    httpParams = httpParams.set("event_attributes2remove", ""+event_attributes2remove);
+    httpParams = httpParams.set("trace_attributes2remove", ""+trace_attributes2remove);
 
     let dia = this.dialog.open(WaitingCircleComponentComponent);
 
